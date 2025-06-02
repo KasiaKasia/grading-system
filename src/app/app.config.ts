c90b92bd3f1f349d90efbd0f1c2150360b1e1e4e
--add <bulicka.alexandra@gmail.com>
2025-06-03 01:50:51 +0200

    [feat] - switch to provideExperimentalZonelessChangeDetection method

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,22 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-// import { provideClientHydration, withHttpTransferCacheOptions } from '@angular/platform-browser';
+import { provideClientHydration, withHttpTransferCacheOptions } from '@angular/platform-browser';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-// import { httpErrorInterceptor } from './core/interceptors/http-error/http-error.interceptor';
+import { httpErrorInterceptor } from './core/interceptors/http-error/http-error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    // provideClientHydration(withHttpTransferCacheOptions({
-    //   includePostRequests: true
-    // })),
+    provideClientHydration(withHttpTransferCacheOptions({
+      includePostRequests: true
+    })),
+    provideExperimentalZonelessChangeDetection(),
     provideAnimations(),
     provideNativeDateAdapter(),
     provideHttpClient(withFetch(),
-      // withInterceptors([httpErrorInterceptor]),
+    withInterceptors([httpErrorInterceptor]),
     )
   ]
 };
