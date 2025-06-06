6e1893c89bdbb4d3c1f725d7a14db5accd763972
--add <bulicka.alexandra@gmail.com>
2025-06-06 15:59:43 +0200

    [fix] - layout mobile

--- src/app/shared/not-found/not-found.component.ts
+++ src/app/shared/not-found/not-found.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { RouterLinkComponent } from '../component/router-link/router-link.component';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, RouterLinkComponent],
   templateUrl: './not-found.component.html',
 })
 export class NotFoundComponent {
